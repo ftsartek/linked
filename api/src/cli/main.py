53dd3ef04c5ae4e2bcefdaa5459aaadaddbcff4f
Jordan Russell <jordan@artek.nz>
2026-01-09 08:45:30 +1300

    feat: add update CLI jobs

--- api/src/cli/main.py
+++ api/src/cli/main.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 import asyncclick as click
 
 from .collect import collect
+from .cron import cron
 from .migrate import migrate
 from .preseed import preseed
 from .schema import schema
@@ -15,6 +16,7 @@ def cli() -> None:
 
 
 cli.add_command(collect)
+cli.add_command(cron)
 cli.add_command(migrate)
 cli.add_command(preseed)
 cli.add_command(schema)
